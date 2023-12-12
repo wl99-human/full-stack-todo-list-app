8c28edd5918fa31efdbc5dc78bd92142826a906e
wl99-human <weileelim2012@gmail.com>
2023-12-12 21:29:28 +0800

    Welcome Component

--- todo-fe/src/app/app.module.ts
+++ todo-fe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
