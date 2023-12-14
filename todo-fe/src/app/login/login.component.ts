cd0acd5c640f3a959e19398206e447d344b75561
wl99-human <weileelim2012@gmail.com>
2023-12-14 16:54:24 +0800

    Redirect to welcome page after successfully login

--- todo-fe/src/app/login/login.component.ts
+++ todo-fe/src/app/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent{
+  username: String = "wl99-human"
+  password = "encrypted"
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
 
+  //Router
+  //Angular.giveMeRouter
+  //Dependency Injection
+  constructor(private router: Router) {
+    //No need declaration like Java
+  }
+
+  handleLogin() {
+    if (this.username === "wl99-human" && this.password === "dummy") {
+      //Redirect to Welcome Page
+      this.router.navigate(['welcome'])
+      this.invalidLogin = false
+    }
+    else {
+      this.invalidLogin = true
+    }
+  }
 }
