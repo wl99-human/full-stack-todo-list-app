d08f7c37c4b10104c4f2bb0923a64c3adf549e88
wl99-human <weileelim2012@gmail.com>
2023-12-20 23:54:54 +0800

    Create a Todo Class and show in todo-list page

--- todo-fe/src/app/list-todos/list-todos.component.ts
+++ todo-fe/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public status: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,9 +18,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    {id: 1, description: 'Learn Angular 17'},
-    {id: 2, description: 'Become Java Spring Expert'},
-    {id: 3, description: 'Develop microservices'}
+    new Todo(1, 'Learn Angular 17', false, new Date()),
+    new Todo(2, 'Become Java Spring Expert', false, new Date()),
+    new Todo(3, 'Develop microservices', false, new Date()),
+    new Todo(4, 'Become a full stack developer', false, new Date())
+    // {id: 1, description: 'Learn Angular 17'},
+    // {id: 2, description: 'Become Java Spring Expert'},
+    // {id: 3, description: 'Develop microservices'}
   ]
 
   constructor() { }
