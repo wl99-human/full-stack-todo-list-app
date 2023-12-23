7913152acfd461df4387ba34d926898094a61d91
wl99-human <weileelim2012@gmail.com>
2023-12-23 23:54:17 +0800

    User Authentication

--- todo-fe/src/app/menu/menu.component.ts
+++ todo-fe/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  constructor(public hardcodedAuthenticationService: HardcodedAuthenticationService) {
+  }
 }
