b5af0d6a6e2e29db55e8fe4aabb2bf412ae982db
wl99-human <weileelim2012@gmail.com>
2023-12-23 22:17:40 +0800

    Hardcoded Authentication Service

---
+++ todo-fe/src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthenticationService {
+
+  constructor() { }
+
+  authenticate(username: string, password: string) {
+    console.log("Before: " + this.isUserLoggedIn());
+    if (username === "wl99-human" && password === "dummy") {
+      sessionStorage.setItem('authenticatedUser', username);
+      console.log("After: " + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser')
+    return !(user === null)
+  }
+}
