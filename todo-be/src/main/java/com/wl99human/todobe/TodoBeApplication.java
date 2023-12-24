5f6074f347cde266794dbc4b535f5f0a05bc7e0f
wl99-human <weileelim2012@gmail.com>
2023-12-24 22:51:08 +0800

    Backend initial commit

---
+++ todo-be/src/main/java/com/wl99human/todobe/TodoBeApplication.java
@@ -0,0 +1,13 @@
+package com.wl99human.todobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBeApplication.class, args);
+	}
+
+}
---
+++ todo-be/src/test/java/com/wl99human/todobe/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl99human.todobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
