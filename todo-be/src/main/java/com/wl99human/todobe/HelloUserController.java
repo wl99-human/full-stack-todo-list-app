00295c45c6e7b1cc7994aa4328811d2b35dfd9b8
wl99-human <weileelim2012@gmail.com>
2023-12-27 00:06:36 +0800

    [Todo-BE] Hello User with path variable

--- todo-be/src/main/java/com/wl99human/todobe/HelloUserController.java
+++ todo-be/src/main/java/com/wl99human/todobe/HelloUserController.java
@@ -1,6 +1,7 @@
 package com.wl99human.todobe;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloUserController {
     @GetMapping(path = "/hello-user-bean")
     public HelloUserBean helloUserBean() {
         return new HelloUserBean("Hello User!");
+    }
+
+    @GetMapping(path = "/hello-user/path-variable/{username}")
+    public HelloUserBean helloUserPathVariable(@PathVariable String username) {
+        return new HelloUserBean(String.format("Hello User, %s", username));
     }
 }
