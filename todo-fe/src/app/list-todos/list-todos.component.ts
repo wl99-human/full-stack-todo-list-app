c665a87249668bbdb1d8c91a837e5c2d4b49032b
wl99-human <weileelim2012@gmail.com>
2024-01-01 22:01:37 +0800

    [Todo-FE] Create new service to subscirbe todo-list

--- todo-fe/src/app/list-todos/list-todos.component.ts
+++ todo-fe/src/app/list-todos/list-todos.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
+import { response } from 'express';
 
 export class Todo {
   constructor(
     public id: number,
+    public username: string,
     public description: string,
-    public status: boolean,
-    public targetDate: Date
+    public targetDate: Date,
+    public status: boolean
   ) {
 
   }
@@ -17,19 +20,21 @@ export class Todo {
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, 'Learn Angular 17', false, new Date()),
-    new Todo(2, 'Become Java Spring Expert', false, new Date()),
-    new Todo(3, 'Develop microservices', false, new Date()),
-    new Todo(4, 'Become a full stack developer', false, new Date())
-    // {id: 1, description: 'Learn Angular 17'},
-    // {id: 2, description: 'Become Java Spring Expert'},
-    // {id: 3, description: 'Develop microservices'}
+  todos: Todo[] = [
+    // new Todo(1, 'Learn Angular 17', false, new Date()),
+    // new Todo(2, 'Become Java Spring Expert', false, new Date()),
+    // new Todo(3, 'Develop microservices', false, new Date()),
+    // new Todo(4, 'Become a full stack developer', false, new Date())
   ]
 
-  constructor() { }
+  constructor(private todoService: TodoDataService) { }
 
   ngOnInit() {
+    this.todoService.retrieveAllTodos("wl99-human").subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      }
+    )
   }
-
 }
---
+++ todo-fe/src/app/service/data/todo-data.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from '../../list-todos/list-todos.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  retrieveAllTodos(username: string) {
+    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`);
+  }
+
+  executeHelloUserBeanServiceWithPathVariable(name: any) {
+    return this.http.get<Todo>(`http://localhost:8080/hello-user/path-variable/${name}`);
+  }
+}
